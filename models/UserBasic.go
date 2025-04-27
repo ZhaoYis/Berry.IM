cbd579aaa98a1e08d5530a4f0f4cb10f984e00bf
大師兄丶 <zhaoyi_dsx@163.com>
2025-04-28 00:37:53 +0800

    feat：配置用户实体

--- models/UserBasic.go
+++ models/UserBasic.go
@@ -5,19 +5,19 @@ import "gorm.io/gorm"
 type UserBasic struct {
 	gorm.Model
 	Uid           string `gorm:"column:uid;type:varchar(36);not null;unique"`
-	Name          string
-	PassWord      string
-	Phone         string
-	Email         string
-	Identity      string
-	ClientIp      string
-	ClientPort    string
-	Salt          string
-	LoginTime     uint64
-	HeartbeatTime uint64
-	LogoutTime    uint64
-	IsLogout      bool
-	DeviceInfo    string
+	Name          string `gorm:"column:name;type:varchar(128);not null"`
+	PassWord      string `gorm:"column:pass_word;type:varchar(128);not null"`
+	Phone         string `gorm:"column:phone;type:varchar(32)"`
+	Email         string `gorm:"column:email;type:varchar(64)"`
+	Identity      string `gorm:"column:identity;type:varchar(128)"`
+	ClientIp      string `gorm:"column:client_ip;type:varchar(36)"`
+	ClientPort    string `gorm:"column:client_port;type:varchar(8)"`
+	Salt          string `json:"salt,type:varchar(100)"`
+	LoginTime     uint64 `json:"login_time,type:uint64"`
+	HeartbeatTime uint64 `json:"heartbeat_time,type:uint64"`
+	LogoutTime    uint64 `json:"logout_time,type:uint64"`
+	IsLogout      bool   `json:"is_logout,type:bool"`
+	DeviceInfo    string `json:"device_info,type:varchar(128)"`
 }
 
 func (UserBasic) TableName() string {
